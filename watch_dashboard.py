9c1d30cb237854298a3028be945c83aa64bdd95f
nsedler <nsedler1@gmail.com>
2023-04-26 11:42:33 -0400

    eda

--- watch_dashboard.py
+++ watch_dashboard.py
@@ -1,11 +1,30 @@
 import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
+from jupyter_dash import JupyterDash
+from dash import Dash, dcc, html, Input, Output
 
-df = pd.read_csv('data/garmin_activities.csv')
+df = pd.read_csv('data/activities_dashboard.csv')
 
-fig = go.Figure(data=[go.Table(
-    header = dict(values=list(df.columns)),
-    cells = dict(values=df.transpose().values.tolist())
-)])
+activities = ['running', 'cycling', 'star_climbing', ]
+df['sport'] = df['sport'].apply(lambda x: x if x in activities else None)
+df_weekly_total_time = df.groupby('week').agg({
+    'total_time' : 'mean'
+}).reset_index()
+print(df_weekly_total_time)
+work_bar_distance_dif = px.histogram(df, x='work_day', y='total_time', histfunc='avg', color='sport')
 
-fig.show()
+weight_loss_line_fig = px.line(df_weekly_total_time, x='week', y='total_time',)
+
+
+
+app = JupyterDash(__name__)
+
+app.layout = html.Div([
+    html.H1('Activity Fitness Dashboard (wip)'),
+    dcc.Graph(figure=work_bar_distance_dif),
+    dcc.Graph(figure=weight_loss_line_fig)
+])
+
+if __name__ == '__main__':
+    app.run_server()
