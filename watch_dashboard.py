12f7d488644df3019723488bf1df7db7ad321e57
nsedler <nsedler1@gmail.com>
2023-05-04 13:06:28 -0400

    eda

--- watch_dashboard.py
+++ watch_dashboard.py
@@ -4,6 +4,13 @@ import pandas as pd
 from jupyter_dash import JupyterDash
 from dash import Dash, dcc, html, Input, Output
 
+def df_to_plotly(df):
+    return {
+        'z' : df['total_time'],
+        'x' : df['week'],
+        'y' : df['day_name']
+    }
+
 df = pd.read_csv('data/activities_dashboard.csv')
 
 activities = ['running', 'cycling', 'star_climbing', ]
@@ -16,6 +23,7 @@ work_bar_distance_dif = px.histogram(df, x='work_day', y='total_time', histfunc=
 
 weight_loss_line_fig = px.line(df_weekly_total_time, x='week', y='total_time',)
 
+time_map = go.Figure(data=go.Heatmap(df_to_plotly(df), colorscale=px.colors.sequential.Greens, ))
 
 
 app = JupyterDash(__name__)
@@ -23,7 +31,8 @@ app = JupyterDash(__name__)
 app.layout = html.Div([
     html.H1('Activity Fitness Dashboard (wip)'),
     dcc.Graph(figure=work_bar_distance_dif),
-    dcc.Graph(figure=weight_loss_line_fig)
+    dcc.Graph(figure=weight_loss_line_fig),
+    dcc.Graph(figure=time_map),
 ])
 
 if __name__ == '__main__':
