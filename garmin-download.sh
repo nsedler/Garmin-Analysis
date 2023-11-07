1b34b78f3967b2a3019846b35deceae93168e4d4
Nate Sedler <nsedler1@gmail.com>
2023-11-07 13:37:57 -0500

    yay it works again!

--- garmin-download.sh
+++ garmin-download.sh
@@ -3,8 +3,8 @@
 garmindb_cli.py --all --download --import --analyze --latest
 echo "garmin data downlaoded"
 
-sqlite3 -header -csv ~/HealthData/DBs/garmin_summary.db "select * from days_summary_view;" > ~/Documents/coding/garmin_analysis/data/garmin_days_summary.csv
+sqlite3 -header -csv ~/HealthData/DBs/garmin_summary.db "select * from days_summary_view;" > ~/Documents/coding/Garmin-Analysis/data/garmin_days_summary.csv
 echo "days summary converted to csv"
-sqlite3 -header -csv ~/HealthData/DBs/garmin_activities.db "select * from activities" > ~/Documents/coding/garmin_analysis/data/garmin_activities.csv
+sqlite3 -header -csv ~/HealthData/DBs/garmin_activities.db "select * from activities" > ~/Documents/coding/Garmin-Analysis/data/garmin_activities.csv
 echo "activities summary converted to csv"
 echo "all done :)"
